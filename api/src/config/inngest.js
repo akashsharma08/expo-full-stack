6952678f34e5a06c344aade6d1d409ca969c391b
Akash Sharma <akash.sharma@techaheadcorp.com>
2026-01-05 15:15:21 +0530

    user schema added

---
+++ api/src/config/inngest.js
@@ -0,0 +1,42 @@
+import { Inngest } from "inngest";
+import connectDB from "./db";
+
+import User from "../models/user.model";
+
+const inngest = new Inngest({
+  id: "ecomm-app",
+});
+
+export default inngest;
+const syncUser = inngest.createFunction(
+  {
+    id: "sync-user",
+  },
+  { event: "clerk/user.created" },
+  async ({ event }) => {
+    await connectDB();
+    const { id, email_addresses, image_url, first_name, last_name } =
+      event.data;
+    const newUser = {
+      clerkId: id,
+      email: email_addresses[0]?.email_address,
+      name: `${first_name || ""} ${last_name || ""}` || "Guest",
+      imageUrl: image_url,
+      addressses: [],
+      wishlist: [],
+    };
+    await User.create(newUser);
+  }
+);
+const deleteUserFromDB = inngest.createFunction(
+  {
+    id: "delete-user-from-db",
+  },
+  { event: "clerk/user.deleted" },
+  async ({ event }) => {
+    await connectDB();
+    const { id } = event.data;
+    await User.deleteOne({ clerkId: id });
+  }
+);
+export const functions = [syncUser, deleteUserFromDB];
--- api/src/server.js
+++ api/src/server.js
@@ -3,12 +3,17 @@ import path from "node:path";
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
 import { clerkMiddleware } from "@clerk/express";
+import { server } from "inngest/express";
+import { functions, inngest } from "./config/inngest.js";
 const app = express();
 const __dirname = path.resolve();
+app.use(express.json());
 app.use(clerkMiddleware());
+app.use("/api/inngest", server({ client: inngest, functions }));
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Health Get api is UP working!!" });
 });
+
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../admin/dist")));
   app.get("/{*any}", (req, res) => {
